03c99d74f857a74ff469f98ee28c5309bb5ae313
John Conley <john@niara.com>
2014-07-10 11:58:07 -0700

    added basic installation and config

---
+++ installation/scripts/install-redis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+installdir=${1:-$HOME}
+
+cd $installdir
+rm -rf redis-stable.tar.gz
+wget http://download.redis.io/redis-stable.tar.gz
+tar xzf redis-stable.tar.gz
+cd redis-stable
+make
+make test
+echo "Redis server is ready to start. Try:"
+echo "src/redis-server $DIR/../conf/redis.conf"
